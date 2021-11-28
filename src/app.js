73ee423c2cf0c97b4409e6fc68637211be17607d
Muhammet Emin Aslangil <eminaslangil@hotmail.com>
2021-11-28 14:04:24 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("merhabaa")
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.50
+}
+console.log(dolarDun)
+let dolarBugun = 9.60
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","x konut kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
